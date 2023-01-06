d3f8b3ba1e5cfaf9c9c5548ef42e9e8fb7743d10
MirrorN <iswuxing@163.com>
2023-01-06 16:10:49 +0800

    add storage function for todolist

--- todo-list/v2.0/action.js
+++ todo-list/v2.0/action.js
@@ -1,37 +1,70 @@
 const form = document.getElementById('form')
 const input = document.getElementById('input')
-const todos = document.getElementById('todos')
+const todoUl = document.getElementById('todos')
 
+/*
+  在 localStorage 中保存的 todos 形式为 string:
+  [{"text":"123","completed":true},{"text":"13333","completed":false}]
+  经 JSON.parse() 处理为数组
+*/
+const todos = JSON.parse(localStorage.getItem('todos'))
+
+
+// 如果 localStorage 中的 todos 项目不为空 则添加到页面中
 if (todos) {
-
+  todos.forEach((item) => {
+    addTodo(item)
+  })
 }
 
-// form 默认 enter 键提交
+// form 中每次提交时 增加新 li
 form.addEventListener('submit', (e) => {
-  // 阻止默认提交事件
   e.preventDefault()
+
   addTodo()
   input.value = ''
 })
 
-function addTodo() {
+function addTodo(todo) {
   let todoText = input.value
+  if (todo) {
+    todoText = todo.text
+  }
   if (todoText) {
-    // 动态创建 li 元素
     const todoLi = document.createElement('li')
+    // 在页面初始化的时候如果localStorage 中的todos不为空并且标记为 completed
+    // 则为 li 项添加 completed 类
+    if (todo && todo.completed) {
+      todoLi.classList.add('completed')
+    }
     todoLi.innerText = todoText
-    todos.appendChild(todoLi)
 
-    // 左键单击改变样式为删除线
+    // 每次点击（改变 completed 或者 删除线 状态都要更新 localStorage 状态）
     todoLi.addEventListener('click', () => {
       todoLi.classList.toggle('completed')
+      keepStorage()
     })
-    // 右键单击为删除
     todoLi.addEventListener('contextmenu', (e) => {
-      // 首先阻止默认右键弹出菜单
       e.preventDefault()
       todoLi.remove()
-
+      keepStorage()
     })
+
+    todoUl.appendChild(todoLi)
   }
+}
+
+/* 
+  用于将当前页面的 li 内容以及是否添加 completed 样式保存至 localStorage 的 todos 项中
+*/
+function keepStorage() {
+  let lis = document.querySelectorAll('li')
+  const todos = []
+  lis.forEach((item) => {
+    todos.push({
+      text: item.innerText,
+      completed: item.classList.contains('completed')
+    })
+  })
+  localStorage.setItem('todos', JSON.stringify(todos))
 }
