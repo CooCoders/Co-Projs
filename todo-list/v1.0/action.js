f4d813e2fd6add3d0d245e8d8e2bf8eaa7dc8b26
MirrorN <iswuxing@163.com>
2023-01-06 11:21:57 +0800

    todolist v1.0

---
+++ todo-list/v1.0/action.js
@@ -0,0 +1,33 @@
+const form = document.getElementById('form')
+const input = document.getElementById('input')
+const todos = document.getElementById('todos')
+
+// form 默认 enter 键提交
+form.addEventListener('submit', (e) => {
+  // 阻止默认提交事件
+  e.preventDefault()
+  addTodo()
+  input.value = ''
+})
+
+function addTodo() {
+  let todoText = input.value
+  if (todoText) {
+    // 动态创建 li 元素
+    const todoLi = document.createElement('li')
+    todoLi.innerText = todoText
+    todos.appendChild(todoLi)
+
+    // 左键单击改变样式为删除线
+    todoLi.addEventListener('click', () => {
+      todoLi.classList.toggle('completed')
+    })
+    // 右键单击为删除
+    todoLi.addEventListener('contextmenu', (e) => {
+      // 首先阻止默认右键弹出菜单
+      e.preventDefault()
+      todoLi.remove()
+
+    })
+  }
+}
