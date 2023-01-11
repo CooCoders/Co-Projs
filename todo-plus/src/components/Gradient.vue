23129ed38cbdccff9fddaae1a5353815231ab14f
MirrorN <iswuxing@163.com>
2023-01-11 17:12:38 +0800

    update todoplus

--- todo-plus/src/components/Gradient.vue
+++ todo-plus/src/components/Gradient.vue
@@ -1,16 +1,24 @@
 <template>
   <div class="gradient">
-    <GradientColor v-for="(todo, index) in todos"></GradientColor>
+    test gradient
+    <!-- <p>{{ todos }}1</p>
+    <p>{{ todos[0] }}2</p> -->
+    <!-- <GradientColor
+      v-for="(todo, index) in todos"
+      :colors="todo.colors"
+      :active="index <= currentIndex"
+    ></GradientColor> -->
   </div>
 </template>
 
 <script setup>
-import { ref, reactive } from 'vue'
+import { ref, reactive, toRef } from 'vue'
 import { useTodoStore } from '../store.js'
 import GradientColor from './GradientColor.vue'
 
 const todoStore = useTodoStore()
-
-const tasks = todoStore.todayTasks
+// const todos = toRef(todoStore.todos)
+// console.log(todos[0])
+// const tasks = todoStore.todayTasks
 </script>
 <style scoped></style>
